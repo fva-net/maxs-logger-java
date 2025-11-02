0a5aa5cae54c01bf6b56980b21cbc91ef6794bac
Koenig Johannes Dr. FRD DIMG6 <johannes.koenig3@zf.com>
2025-11-02 14:02:34 +0100

    Extended MaxsMessageType with indication of debug messages

--- src/main/java/de/fva_net/maxs/logger/MaxsMessageType.java
+++ src/main/java/de/fva_net/maxs/logger/MaxsMessageType.java
@@ -1,48 +1,58 @@
 package de.fva_net.maxs.logger;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * Enumeration representing message types.
  */
+@Getter
+@AllArgsConstructor
 public enum MaxsMessageType {
 	/**
      * Represents an error message type.
 	 * <p>
 	 * Severity: High. Indicates a failure or critical issue.
      */
-    ERROR,
+	ERROR(false),
 
     /**
      * Represents a warning message type.
 	 * <p>
 	 * Severity: Medium. Indicates a potential problem or non-critical issue.
      */
-    WARNING,
+	WARNING(false),
 
     /**
      * Represents an info message type.
 	 * <p>
 	 * Severity: Low. Provides informational messages about normal operations.
      */
-    INFO,
+	INFO(false),
 
 	/**
 	 * Represents a debug error message type.
 	 * <p>
 	 * Severity: High (Debug). Used for debugging critical errors.
      */
-    DEBUG_ERROR,
+	DEBUG_ERROR(true),
 
     /**
      * Represents a debug warning message type.
 	 * <p>
 	 * Severity: Medium (Debug). Used for debugging potential issues.
      */
-    DEBUG_WARNING,
+	DEBUG_WARNING(true),
 
     /**
      * Represents a debug info message type.
 	 * <p>
 	 * Severity: Low (Debug). Used for debugging informational messages.
      */
-    DEBUG_INFO
+	DEBUG_INFO(true);
+
+	/**
+	 * Indicates whether this message type is for debugging purposes.
+	 */
+	private final boolean debug;
 }
