f86fd5c5552888e5d4e5d374ec39c3fce468137d
Koenig Johannes Dr. FRD DIMG6 <johannes.koenig3@zf.com>
2025-11-02 14:02:34 +0100

    Extended MaxsMessageType with indication of debug messages

---
+++ src/test/java/de/fva_net/maxs/logger/MaxsMessageTypeTest.java
@@ -0,0 +1,20 @@
+package de.fva_net.maxs.logger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MaxsMessageTypeTest {
+
+	@Test
+	void isDebug() {
+		assertFalse(MaxsMessageType.ERROR.isDebug());
+		assertFalse(MaxsMessageType.WARNING.isDebug());
+		assertFalse(MaxsMessageType.INFO.isDebug());
+		assertTrue(MaxsMessageType.DEBUG_ERROR.isDebug());
+		assertTrue(MaxsMessageType.DEBUG_WARNING.isDebug());
+		assertTrue(MaxsMessageType.DEBUG_INFO.isDebug());
+	}
+
+}
